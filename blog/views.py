0b727a697b87130de8f91a7335804902da0aa024
SwitchCaseKam <kamil.leczycki.ns@gmail.com>
2018-10-21 23:05:04 +0200

    prototype of django page

---
+++ blog/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
+
+posts = [
+    {
+        'author': 'Kamil',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'October 17, 2018'
+    },
+    {
+        'author': 'Borys',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 18, 2018'
+    }
+]
+# Create your views here.
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
