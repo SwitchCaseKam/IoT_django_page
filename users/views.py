94b724d3ad31c1e7c28d65c354cc5676abb7d17c
SwitchCaseKam <kamil.leczycki.ns@gmail.com>
2018-11-17 22:18:31 +0100

    update user profile, required login in analysis page

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 from blog.models import Drinking, Activity
 from django.contrib.auth.models import User
@@ -23,15 +23,24 @@ def register(request):
 
 @login_required
 def profile(request):
-    current_user_id = User.objects.filter(username=request.user).first().id
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,
+                                   request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
     context = {
-        #'posts': Post.objects.all()
-        #'stats': Stats.objects.all()
-        #'tests': Test.objects.using('smartband_database').all(),
-
-        'drinking': Drinking.objects.using('new_smartband_db').filter(user=current_user_id).order_by('-timestamp'),
-        'activity': Activity.objects.using('new_smartband_db').filter(user=current_user_id).order_by('-timestamp')
-
-
+        'u_form': u_form,
+        'p_form': p_form
     }
+
     return render(request, 'users/profile.html', context)
